69aa86a3770ac407adabd03092cde99aa135fb4a
Gabriel Tartaglia <ga.tartaglia96@gmail.com>
2016-01-05 10:12:30 -0200

    Cria app Express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict';
+var express = require('express')
+  , app     = express();
+
+var port = 3000;
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res) {
+  res.send('Ok');
+});
+
+app.listen(port);
