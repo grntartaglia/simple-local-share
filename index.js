80eceb417b30f91a03138059342ffca326372aa2
Gabriel Tartaglia <ga.tartaglia96@gmail.com>
2016-01-07 11:04:46 -0200

    Separa módulo walk

--- index.js
+++ index.js
@@ -1,50 +1,13 @@
 'use strict';
 
 const express = require('express');
-const filesize = require('file-size');
 const ip = require('ip');
 const engines = require('consolidate');
-const fs = require('fs');
 const path = require('path');
+const walk = require('./lib/walk');
 const app = express();
 const port = process.argv[2] || 3000;
 const shareFolder = 'share';
-
-function walk(dir, done) {
-  let results = [];
-
-  fs.readdir(dir, (dirErr, list) => {
-    if (dirErr) return done(dirErr);
-
-    let pending = list.length;
-    if (!pending) return done(null, results);
-
-    list.forEach((file) => {
-      // Ignore .gitkeep and .DS_Store
-      if (file === '.gitkeep' || file === '.DS_Store') {
-        if (!--pending) return done(null, results);
-        return false;
-      }
-
-      const filePath = path.join(dir, file);
-
-      fs.stat(filePath, (statErr, stat) => {
-        if (stat && stat.isDirectory()) {
-          walk(filePath, (err, res) => {
-            results = results.concat(res);
-            if (!--pending) return done(null, results);
-          });
-        } else {
-          const name = filePath;
-          const size = filesize(stat.size).human();
-
-          results.push({ name, size });
-          if (!--pending) return done(null, results);
-        }
-      });
-    });
-  });
-}
 
 app.engine('mustache', engines.mustache);
 
